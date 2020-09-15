9afb8f4d2be528f3441ca6f2829e9743ae4c43d6
Matthew_Sharp <matthew.sharp1@ontariotechu.net>
2020-09-15 11:17:54 -0400

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Hello
 {
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println("Please input your age: ");
+        Scanner input = new Scanner(System.in);
+        int age = input.nextInt();
+        System.out.printf("Your age is: %s", age);
     }
 }
